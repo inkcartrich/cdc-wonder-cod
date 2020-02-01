07aeee91990be25c067c664413b13b220fadd6a2
Carter Richard <58718246+deltatea@users.noreply.github.com>
2020-01-31 20:06:24 -0500

    Backing up application to github

---
+++ app.R
@@ -0,0 +1,137 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(ggplot2)
+library(shinyWidgets)
+library(shinythemes)
+library(tidyverse)
+library(cowplot)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+    
+    theme = shinytheme("cyborg"),
+    
+    # Application title
+    titlePanel("CDC WONDER Underlying Cause of Death"),
+ 
+    sidebarLayout(
+        sidebarPanel(
+            helpText("Dataset for mortality as a result of mental and behavioral disorders from 1999 - 2017 
+                     in the United States."),
+            selectInput("state", "State", ""),
+            selectInput("cod", "Cause of Death", ""),
+            
+            radioButtons("vis", "Visualization", c("Mortality" = "Deaths",
+                                                   "Crude Rate" = "Crude.Rate"),
+                         selected = "Deaths"),
+            
+            helpText("Showing top five counties:"),
+            
+            fluidRow(
+                tableOutput("contents")
+                )
+    ),
+        # Show a plot of the generated distribution
+        mainPanel(
+           plotOutput("distPlot")
+        )
+    ),
+    
+
+)
+
+server <- function(input, output, session) {
+    
+
+    myData <- reactive({
+        read.delim("cod_county_2017.txt")
+    })
+    
+    observe({
+        updateSelectInput(session, "state",
+                          label = "State",
+                          choices = c("All", unique(as.character(myData()[["State"]]))),
+                          selected = "All")
+        
+        updateSelectInput(session, "cod",
+                          label = "Cause of Death",
+                          choices = c("All", unique(as.character(myData()[["Cause.of.death"]]))),
+                          selected = "All")
+    })
+    
+    myDataFilt <- reactive({
+                    
+                    selection <- input[["vis"]]
+                    
+                    myData() %>% 
+                        filter(
+                            (if(input[["state"]] != "All"){State == input[["state"]]}
+                             else{State == State}),
+                            (if(input[["cod"]] != "All"){Cause.of.death == input[["cod"]]}
+                             else{Cause.of.death == Cause.of.death})
+                        ) %>% 
+                    select(fips = County.Code, value = selection) %>% 
+                    filter(!is.na(fips), value != "Unreliable") %>% 
+                    group_by(fips) %>% 
+                    summarize(tot = sum(as.numeric(value))) %>% 
+                    as.data.frame()
+    })
+    
+    output[["distPlot"]] <- renderPlot({
+        x    <- myDataFilt()
+        state <- if(input[["state"]] != "All"){input[["state"]]}
+                    else{}
+        state_title <- if(input[["state"]] != "All"){input[["state"]]}
+                        else{"the United States"}
+        cod_title <- if(input[["cod"]] != "All"){tolower(paste("from", input[["cod"]]))}
+                        else{"due to mental and behavioral disorders"}
+        select_title <- if(input[["vis"]] == "Deaths"){"Mortality"}else{"Crude death rate"}
+            
+        usmap::plot_usmap(regions = "counties", data = x, values = "tot", color = NA, include = as.character(state)) +
+            scale_fill_continuous(
+                low = "navy", high = "orange", name = "Total"
+            ) +
+            labs(title = paste(select_title, "in",  state_title, cod_title, "(1999 - 2017)"),
+                 subtitle = "All sub-national mortality and population figures less than 10 persons are suppressed (shown in gray)",
+                 caption = "CDC WONDER Underlying Cause of Death") +
+            theme(
+                text = element_text(color = "white"),
+                legend.background = element_rect(fill = "black"),
+                plot.background = element_rect(fill = "black"),
+            )
+    }, width = "auto", bg = "black")
+    
+    output[["contents"]] <- renderTable({
+        
+        selection <- input[["vis"]]
+        
+        d <- myData() %>% 
+            filter(
+                (if(input[["state"]] != "All"){State == input[["state"]]}
+                 else{State == State}),
+                (if(input[["cod"]] != "All"){Cause.of.death == input[["cod"]]}
+                 else{Cause.of.death == Cause.of.death})
+            ) %>% 
+            select(fips = County, value = selection) %>% 
+            filter(!is.na(fips), value != "Unreliable") %>% 
+            group_by(fips) %>% 
+            summarize(tot = sum(as.integer(value))) %>% 
+            arrange(desc(tot)) %>% 
+            head(5)
+        
+        colnames(d) <- c("County, State",
+                         "Value")
+        d
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
---
+++ vis.R
@@ -0,0 +1,43 @@
+library(dplyr)
+library(ggplot2)
+library(maps)
+
+cod_county_2017 <- read.delim("cod_county_2017.txt")
+
+by_county <- cod_county_2017 %>% 
+  select(fips = County.Code, Deaths) %>% 
+  filter(!is.na(fips)) %>% 
+  group_by(fips) %>% 
+  summarize(tot_deaths = sum(Deaths)) %>% 
+  as.data.frame()
+
+p1 <- usmap::plot_usmap(regions = "counties", data = by_county, values = "tot_deaths", color = NA) +
+  scale_fill_continuous(
+    low = "navy", high = "orange", name = "Total Deaths"
+  ) +
+  labs(title = "Total deaths in the United States due to mental and behavioral disorders (1999 - 2017)",
+       subtitle = "Mortality figures less than 10 persons are suppressed (shown in gray)",
+       caption = "CDC WONDER Underlying Cause of Death ") +
+  theme(
+    text = element_text(color = "white"),
+    legend.background = element_rect(fill = "black"),
+    plot.background = element_rect(fill = "black"),
+    plot.margin=grid::unit(c(0,0,0,0), "mm")
+  )
+
+ggsave("county_plot.png", p1)
+
+p2 <- usmap::plot_usmap(regions = "counties", data = by_county, values = "tot_deaths", color = NA, include = "IL") +
+  scale_fill_continuous(
+    low = "navy", high = "orange", name = "Total Deaths"
+  ) +
+  labs(title = "Deaths due to mental and behavioral disorders in Illinois (1999 - 2017)",
+       subtitle = "Mortality figures less than 10 persons are suppressed (shown in gray)",
+       caption = "CDC WONDER Underlying Cause of Death ") +
+  theme(
+    text = element_text(color = "white"),
+    legend.background = element_rect(fill = "black"),
+    plot.background = element_rect(fill = "black", color = NA)
+  )
+
+ggsave("il_plot.png", p2, width = 10, height = 10)
